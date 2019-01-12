23d40749e5a3e2b9fac722e283d18f5e9ae92ab1
Eliana Levenson <elianalevenson@Elianas-MacBook-Air.local>
2019-01-12 14:04:08 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,20 @@
 console.log("Up & running!");
 
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ["king", "king", "queen", "queen"];
+var cardsInPlay = [];
 
-console.log ("User flipped " + cardTwo);
-console.log ("User flipped " + cardThree);
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay [1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again!");
+  }
+}
